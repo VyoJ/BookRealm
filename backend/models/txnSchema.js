430cac64b05381dcbec04c1e3e6f1ee82b82de8e
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2024-03-26 20:23:42 +0530

    Transaction changes

---
+++ backend/models/txnSchema.js
@@ -0,0 +1,12 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+let txnSchema = new Schema({
+  userid: { type: String, required: true },
+  bookid: { type: String, required: true },
+  type: { type: String, required: true },
+  amount: { type: String, required: true },
+  date: { type: Date, required: true },
+});
+
+module.exports = mongoose.model("Transaction", txnSchema);
--- bookrealm/src/components/layouts/navbar/Navbar.js
+++ bookrealm/src/components/layouts/navbar/Navbar.js
@@ -80,20 +80,16 @@ export default function Navbar({ darkTheme, darkTextTheme }) {
     setIsMenuOpen(!isMenuOpen);
   };
 
-
-
-    const imageData = localStorage.getItem('selectedFile');
-  
-
+  const imageData = localStorage.getItem("selectedFile");
 
   const handelLogout = () => {
     signOut(auth)
       .then(() => {
         localStorage.removeItem("userId");
-        localStorage.removeItem('selectedFile')
+        localStorage.removeItem("selectedFile");
         console.log("logout successful");
         navigate("/");
-        console.log(user,'after logout')
+        console.log(user, "after logout");
       })
       .catch((error) => {
         console.log(error);
@@ -141,14 +137,15 @@ export default function Navbar({ darkTheme, darkTextTheme }) {
               </Link>
               <Link
                 to="/cart"
-                className={`${darkTextTheme
-                  ? "nav-links-dark cart-link"
-                  : "nav-links cart-link"
-                  } `}
+                className={`${
+                  darkTextTheme
+                    ? "nav-links-dark cart-link"
+                    : "nav-links cart-link"
+                } `}
               >
                 <FontAwesomeIcon icon={faCartShopping} />
               </Link>
-              <div className=" ml-5" style={{color:"white"}}>
+              <div className=" ml-5" style={{ color: "white" }}>
                 <p>hi , User </p>
               </div>
             </>
@@ -168,30 +165,49 @@ export default function Navbar({ darkTheme, darkTextTheme }) {
               </Link>
             </>
           )}
-                 <Link
-                to={`${user ? '/user':'/login'}`}
-                onClick={() => {
-                  if(!user) {
-                    alert('Please Login to view your profile')
-                  }
-                }}
-                className={`${darkTextTheme
-                  ? "nav-links-dark cart-link"
-                  : "nav-links cart-link"
-                  } `}
+          <Link
+            to={`${user ? "/user" : "/login"}`}
+            onClick={() => {
+              if (!user) {
+                alert("Please Login to view your profile");
+              }
+            }}
+            className={`${
+              darkTextTheme ? "nav-links-dark cart-link" : "nav-links cart-link"
+            } `}
+          >
+            <div className="relative ml-3">
+              <button
+                type="button"
+                className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                id="user-menu-button"
+                aria-expanded="false"
+                aria-haspopup="true"
               >
-                <div className="relative ml-3">
-                  <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                    <span className="absolute -inset-1.5"></span>
-                    <span className="sr-only">Open user menu</span>
-                    {user? 
-                    <img className="h-8 w-8 rounded-full" src={imageData ? imageData:"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"} alt="" /> :
-                    <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                <span className="absolute -inset-1.5"></span>
+                <span className="sr-only">Open user menu</span>
+                {user ? (
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src={
+                      imageData
+                        ? imageData
+                        : "https://w7.pngwing.com/pngs/717/24/png-transparent-computer-icons-user-profile-user-account-avatar-heroes-silhouette-black-thumbnail.png"
+                        // : "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     }
-                  </button>
-                </div>
-              </Link>
-
+                    alt=""
+                  />
+                ) : (
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://w7.pngwing.com/pngs/717/24/png-transparent-computer-icons-user-profile-user-account-avatar-heroes-silhouette-black-thumbnail.png"
+                    // src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                )}
+              </button>
+            </div>
+          </Link>
         </nav>
       </div>
     </section>
