594d37144279d7ad42bf4830fc243a27a87a60af
--get <chakravarthimithun62@gmail.com>
2024-03-31 18:21:41 +0530

    cart item bought status

--- bookrealm/src/components/layouts/cartitem-container/CartItemContainer.js
+++ bookrealm/src/components/layouts/cartitem-container/CartItemContainer.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./CartItemContainer.style.css";
 import { cartContext } from "../../../App";
 import CartItemCard from "../../cards/cart-item-cart/CartItemCard";
@@ -9,14 +9,22 @@ export const CartItemContainer = () => {
   const { cartItem, totalAmount } = useContext(cartContext);
   const stripeKey = "pk_test_51OzK3tSF4U7blLf0thrL3ZFYuWz3am5wArcUroVJAtyzh8msqN2m2yxljQPJReHQnVvUvyMEp58Jbr3sqNMvkRID00XmVUg2SJ"
   const navigate = useNavigate()
+  let itemBought = null
+  let Token = null
+  const [Bought, setBought] = useState([]);
+  console.log(itemBought,"itemBought")
 
-  const onToken = (token) => {
+  const onToken = async (token) => {
     //the actuall payment to be handel in the backend from here
+    Token = token
     console.log(token)
+    console.log(Token, "Token")
     alert('your payment has been processed')
+    itemBought = cartItem
     navigate('/books')
-
+    setBought([cartItem])
   }
+  console.log(Bought)
   // console.log("cartitem container");
   // console.log(cartItem);
   return (
@@ -24,26 +32,27 @@ export const CartItemContainer = () => {
       <div className="container">
         {totalAmount === 0 ? (
           <h2 className="text-primary text-center mt-14 mb-14">Currently your cart is empty</h2>
-        ):(
+        ) : (
           <React.Fragment>
             <h2 className="text-primary">Cart</h2>
-          {cartItem.map((item) => (
-            <CartItemCard key={item.id} bookdata={item} />
-          ))}
-          <h2 className="text-primary">Total Amount = &#8377;{totalAmount}</h2>
-          <StripeCheckout
-            name="Book Checkout"
-            description="Please fill in the details below"
-            amount={totalAmount*100}
-            currency="INR"
-            stripeKey={stripeKey}
-            token={onToken}
-            billingAddress
-          >
-          <button className="button-primary"><small>Proceed to Checkout</small></button>
+            {cartItem.map((item) => (
+              <CartItemCard key={item.id} bookdata={item} />
 
-          </StripeCheckout>
-          
+            ))}
+            <h2 className="text-primary">Total Amount = &#8377;{totalAmount}</h2>
+            <StripeCheckout
+              name="Book Checkout"
+              description="Please fill in the details below"
+              amount={totalAmount * 100}
+              currency="INR"
+              stripeKey={stripeKey}
+              token={onToken}
+              billingAddress
+            >
+              <button className="button-primary"><small>Proceed to Checkout</small></button>
+
+            </StripeCheckout>
+
           </React.Fragment>
         )}
       </div>
