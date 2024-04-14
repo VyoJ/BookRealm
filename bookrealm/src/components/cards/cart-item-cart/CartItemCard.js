e6e6090e7b7fff7755bbbe75be186b0e71147394
Vyoman Jain <75789232+VyoJ@users.noreply.github.com>
2024-04-14 16:23:32 +0530

    Small changes to cart

--- bookrealm/src/components/cards/cart-item-cart/CartItemCard.js
+++ bookrealm/src/components/cards/cart-item-cart/CartItemCard.js
@@ -1,24 +1,24 @@
 import React, { useContext, useState } from "react";
 import "./cart-item-card.style.css";
 import { cartContext } from "../../../App";
-import { useLocation, useNavigate  } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import CartBackendContext from "../../../pages/context/CartBackendContext";
 
 const CartItemCard = ({ bookdata }) => {
-  const { deleteCartItem } = useContext(CartBackendContext)
-  const location = useLocation()
-  const navigate = useNavigate()
+  const { deleteCartItem } = useContext(CartBackendContext);
+  const location = useLocation();
+  const navigate = useNavigate();
   // console.log(location)
   // const { cartItem, setcartItem } = useContext(cartContext);
   // const [options, setoptions] = useState(location.state ? location.state.options : {})
   // const  order_type = location.state ? location.state.type : ''
-  const order_type = bookdata.order_type
+  const order_type = bookdata.order_type;
   // console.log(bookdata,'bookdata form cartcard')
   // console.log(order_type)
   // console.log(bookdata,'from cartcard');
 
   const handleRemove = () => {
-    deleteCartItem(bookdata._id)
+    deleteCartItem(bookdata._id);
   };
 
   return (
@@ -28,26 +28,30 @@ const CartItemCard = ({ bookdata }) => {
           src={bookdata.image}
           alt="cart-item-img"
           className="cart-item-img"
-          onClick={() =>     navigate(`/book-details/${bookdata.bookid}`)}
+          onClick={() => navigate(`/book-details/${bookdata.bookid}`)}
         />
       </div>
       <div className="cart-item-content-container">
-        <h2 className="text-primary"><b>{bookdata.title}</b>,<small style={{ fontSize: "smaller" }}>{bookdata.authors}</small></h2>
+        <h2 className="text-primary">
+          <b>{bookdata.title}</b>,
+          <small style={{ fontSize: "smaller" }}>{bookdata.authors}</small>
+        </h2>
         <p>Book-Type : {bookdata.type}</p>
         <p>Order-Type : {order_type}</p>
         {/* <p>date : {bookdata.date}</p> */}
-        {order_type === 'Buy' ?
+        {order_type === "Buy" ? (
           //  <p>Quantity : {options.quantity}</p>
-          //   : 
+          //   :
           //  <p>Hours : {options.hr}</p>
           <p>Quantity : {bookdata.rent_period}</p>
-          :
+        ) : (
           <p>Hours : {bookdata.rent_period}</p>
-        }
-        <h3 className="cart-item-price"><b>&#8377;{bookdata.price}</b></h3>
+        )}
+        <h3 className="cart-item-price">
+          <b>&#8377;{bookdata.price}</b>
+        </h3>
         <div className="cart-btn-container">
           <div className="cart-btn-part">
-
             <button onClick={handleRemove} className="delete_button">
               Remove from Cart
             </button>
--- bookrealm/src/pages/context/CartState.js
+++ bookrealm/src/pages/context/CartState.js
@@ -1,59 +1,69 @@
-import React,{useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import CartBackendContext from "./CartBackendContext";
-import axios  from "axios";
-import { userContext,cartContext } from "../../App";
+import axios from "axios";
+import { userContext, cartContext } from "../../App";
 
-const CartState = ( props) => {
-    const {cartItem,setcartItem } = useContext(cartContext)
-    const host = "http://localhost:2000/cart"
-    // const cartinitial = []
-    // const [cart,setcart] = useState(cartinitial)
-    const authenticateUser = useContext(userContext)
-    // console.log(authenticateUser.uid)
-    // console.log('this is cart' ,cartItem)
+const CartState = (props) => {
+  const { cartItem, setcartItem } = useContext(cartContext);
+  const host = "http://localhost:2000/cart";
+  // const cartinitial = []
+  // const [cart,setcart] = useState(cartinitial)
+  const authenticateUser = useContext(userContext);
+  // console.log(authenticateUser.uid)
+  // console.log('this is cart' ,cartItem)
 
-    const getcartItem = async() => {
-        const response = await axios.get(`${host}/${authenticateUser.uid}`)    
-        setcartItem(response.data)  
-        console.log('cart item successfully fetched',response.data)
-    } 
+  const getcartItem = async () => {
+    const response = await axios.get(`${host}/${authenticateUser.uid}`);
+    setcartItem(response.data);
+    console.log("cart item successfully fetched", response.data);
+  };
 
-
-    const addCartItem = async (book_id,type,amount,time,image,author,title,order_type) => {
-        try {
-            const cartItemData = {
-                userid: authenticateUser.uid, 
-                bookid: book_id, 
-                type:type, 
-                price: amount, 
-                rent_period: time, 
-                image: image, 
-                authors: author,
-                title: title,
-                order_type:order_type
-            };
-            const response = await axios.post(`${host}/add`, cartItemData);
-            console.log(response.data,'item added to cart successfully'); 
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    const deleteCartItem = async(id) => {
-        try {
-            const response= await axios.delete(`${host}/delete/${id}`)
-            const  updatecart = cartItem.filter(item => item._id !== id)
-            setcartItem(updatecart)
-            console.log(response.data,'cartitem successfully deleted')
-        } catch (error) {
-            console.error(error)
-        }
+  const addCartItem = async (
+    book_id,
+    type,
+    amount,
+    time,
+    image,
+    author,
+    title,
+    order_type
+  ) => {
+    try {
+      const cartItemData = {
+        userid: authenticateUser.uid,
+        bookid: book_id,
+        type: type,
+        price: amount,
+        rent_period: time,
+        image: image,
+        authors: author,
+        title: title,
+        order_type: order_type,
+      };
+      const response = await axios.post(`${host}/add`, cartItemData);
+      console.log(response.data, "item added to cart successfully");
+    } catch (error) {
+      console.error(error);
     }
+  };
+  const deleteCartItem = async (id) => {
+    try {
+      const response = await axios.delete(`${host}/delete/${id}`);
+      const updatecart = cartItem.filter((item) => item._id !== id);
+      setcartItem(updatecart);
+      console.log(response.data, "cartitem successfully deleted");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return(
-        <CartBackendContext.Provider value={{getcartItem,addCartItem,deleteCartItem}}>
-            {props.children}
-        </CartBackendContext.Provider>
-    )
-}
+  return (
+    <CartBackendContext.Provider
+      value={{ getcartItem, addCartItem, deleteCartItem }}
+    >
+      {props.children}
+    </CartBackendContext.Provider>
+  );
+};
 
 export default CartState;
